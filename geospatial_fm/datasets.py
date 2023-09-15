9cafbb270732a770790ca9c6bbf486158ab5ed6d
samKhallaghi <samkhallaghi@gmail.com>
2023-09-14 22:40:48 -0400

    Work in Progress

--- geospatial_fm/datasets.py
+++ geospatial_fm/datasets.py
@@ -23,3 +23,61 @@ class GeospatialDataset(CustomDataset):
             **kwargs)
 
         self.gt_seg_map_loader = LoadGeospatialAnnotations(reduce_zero_label=reduce_zero_label, **gt_seg_map_loader_cfg)
+
+
+from mmpretrain.registry import DATASETS
+from mmpretrain.datasets.base_dataset import BaseDataset
+from typing import List, Any
+
+@DATASETS.register_module()
+class MultiLabelDataset(BaseDataset):
+    """Multi-label Dataset for image classification.
+
+    This dataset extends BaseDataset to support multi-label classification.
+
+    Args:
+        same arguments as BaseDataset
+    
+    The annotation format is shown as follows.
+
+    .. code-block:: none
+
+        {
+            "metainfo":
+            {
+              "classes": ['A', 'B', 'C', ...]
+            },
+            "data_list":
+            [
+              {
+                "img_path": "test_img1.jpg",
+                'gt_label': [0, 1],
+              },
+              {
+                "img_path": "test_img2.jpg",
+                'gt_label': [2],
+              },
+            ],
+            ...
+        }
+    """
+    def __init__(self, **kwargs: Any):
+        super().__init__(**kwargs)
+        # You can put any custom initialization here
+
+    def get_cat_ids(self, idx: int) -> List[int]:
+        """Get category ids by index.
+
+        Args:
+            idx (int): Index of data.
+
+        Returns:
+            List[int]: Image categories of specified index.
+
+        Raises:
+            KeyError: If 'gt_label' does not exist in data_info
+        """
+        data_info = self.get_data_info(idx)
+        if 'gt_label' not in data_info:
+            raise KeyError(f"'gt_label' not found in data_info for index {idx}")
+        return data_info['gt_label']
--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     install_requires=
     [
         "mmsegmentation @ git+https://github.com/open-mmlab/mmsegmentation.git@186572a3ce64ac9b6b37e66d58c76515000c3280",
+        "mmpretrain @ git+https://github.com/open-mmlab/mmpretrain.git",
         "rasterio",
         "tifffile",
         "einops",
